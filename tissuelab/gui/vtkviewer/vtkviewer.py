9c4b41335b3ceff9bb746bf8a4a86f381a680b7d
Guillaume Baty <guillaume.baty@inria.fr>
2015-01-29 14:57:20 +0100

    VtkViewer now support cut planes

--- tissuelab/gui/vtkviewer/vtkviewer.py
+++ tissuelab/gui/vtkviewer/vtkviewer.py
@@ -9,10 +9,88 @@ from openalea.core.service.ipython import interpreter as get_interpreter
 from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
 
-
 def expand(widget):
     p = QtGui.QSizePolicy
     widget.setSizePolicy(p(p.MinimumExpanding, p.MinimumExpanding))
+
+
+def define_LUT(image, LUT_type):
+    lut = vtk.vtkLookupTable()
+    if LUT_type == "bw":
+        lut.SetTableRange(image.min(), image.max())
+        lut.SetSaturationRange(0, 0)
+        lut.SetHueRange(0, 0)
+        lut.SetValueRange(0, 1)
+    elif LUT_type == "color":
+        lut.SetNumberOfTableValues(image.max() + 1)
+        lut.SetNumberOfColors(image.max() + 1)
+        lut.SetRange(image.min(), image.max())
+    lut.Build()
+    return lut
+
+
+def matrix_to_image_reader(name, data_matrix, datatype=np.uint16, decimate=1):
+    nx, ny, nz = data_matrix.shape
+    data_string = data_matrix.tostring('F')
+
+    reader = vtk.vtkImageImport()
+    reader.CopyImportVoidPointer(data_string, len(data_string))
+    if datatype == np.uint8:
+        reader.SetDataScalarTypeToUnsignedChar()
+    else:
+        reader.SetDataScalarTypeToUnsignedShort()
+    reader.SetNumberOfScalarComponents(1)
+    reader.SetDataExtent(0, nx - 1, 0, ny - 1, 0, nz - 1)
+    reader.SetWholeExtent(0, nx - 1, 0, ny - 1, 0, nz - 1)
+
+    return reader
+
+
+def blend_funct(data_matrix, data1, LUT1, data2, LUT2, orientation):
+    nx, ny, nz = data_matrix.shape
+    xMax = nx - 1
+    yMax = ny - 1
+    zMax = nz - 1
+
+    p1Colors = vtk.vtkImageMapToColors()
+    p1Colors.SetInputConnection(data1.GetOutputPort())
+    p1Colors.SetLookupTable(LUT1)
+    p2Colors = vtk.vtkImageMapToColors()
+    p2Colors.SetInputConnection(data2.GetOutputPort())
+    p2Colors.SetLookupTable(LUT2)
+    blend = vtk.vtkImageBlend()
+    blend.AddInput(p1Colors.GetOutput())
+    blend.AddInput(p2Colors.GetOutput())
+    blend.SetOpacity(0, 0.5)
+    blend.SetOpacity(1, 0.5)
+    imgactor = vtk.vtkImageActor()
+    imgactor.SetInput(blend.GetOutput())
+    if orientation == 1:
+        imgactor.SetDisplayExtent(np.round(xMax / 2), np.round(xMax / 2), 0, yMax, 0, zMax)
+    elif orientation == 2:
+        imgactor.SetDisplayExtent(0, xMax, 0, yMax, np.round(zMax / 2), np.round(zMax / 2))
+    elif orientation == 3:
+        imgactor.SetDisplayExtent(0, xMax, np.round(yMax / 2), np.round(yMax / 2), 0, zMax)
+
+    imgactor.SetOrigin(nx / 2., ny / 2., nz / 2.)
+    imgactor.SetPosition(-(nx - 1) / 2., -(ny - 1) / 2., -(nz - 1) / 2.)
+
+    return imgactor, blend
+
+
+class VtkControlPanel(QtGui.QWidget):
+
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self)
+        self._layout = QtGui.QVBoxLayout(self)
+
+        from openalea.oalab.service.qt_control import qt_editor
+        from openalea.core.control import Control
+        x = Control('x', 'IInt', 0)
+        x.interface.min = 0
+        x.interface.max = 255
+        editor = qt_editor(x)
+        self._layout.addWidget(editor)
 
 
 class VtkViewerWidget(QtGui.QWidget, AbstractListener):
@@ -42,7 +120,7 @@ class VtkViewerWidget(QtGui.QWidget, AbstractListener):
         for obj_name, world_object in world.items():
             obj = world_object.obj
             if isinstance(obj, np.ndarray):
-                self.vtk.add_matrix(obj_name, obj)
+                self.vtk.add_matrix(obj_name, obj, datatype=obj.dtype)
             if isinstance(obj, vtk.vtkActor):
                 self.vtk.add_actor(obj_name, obj)
         self.vtk.compute()
@@ -80,6 +158,15 @@ class VtkViewer(QtGui.QWidget):
         self.volume = {}
         self.actor = {}
 
+    def display_volume(self, disp=True):
+        self.volume.clear()
+        self.volume_property.clear()
+        if disp:
+            for name, data_matrix in self.matrix.items():
+                self.add_matrix_as_volume(name, data_matrix, data_matrix.dtype)
+        else:
+            pass
+
     def initialize(self):
         pass
 #         self.ren.ResetCamera()
@@ -106,6 +193,33 @@ class VtkViewer(QtGui.QWidget):
         self.volume_property = {}
         self.volume = {}
         self.actor = {}
+        self.vtkdata = {}
+
+    def move_cut_plane(self, name, position=0, orientation=1):
+        actor = self.actor['%s_cut_plane_%d' % (name, orientation)]
+        data_matrix = self.matrix[name]
+        nx, ny, nz = data_matrix.shape
+
+        xMax = nx - 1
+        yMax = ny - 1
+        zMax = nz - 1
+        bounds = [xMax, yMax, zMax]
+
+        if position > bounds[orientation - 1]:
+            position = bounds[orientation - 1]
+        elif position < bounds[orientation - 1]:
+            position = 0
+
+        if orientation == 1:
+            actor.SetDisplayExtent(position, position, 0, yMax, 0, zMax)
+
+        elif orientation == 2:
+            actor.SetDisplayExtent(0, xMax, position, position, 0, zMax)
+
+        elif orientation == 3:
+            actor.SetDisplayExtent(0, xMax, 0, yMax, position, position)
+
+        self.render()
 
     def refresh(self):
         self.compute()
@@ -127,18 +241,22 @@ class VtkViewer(QtGui.QWidget):
     def add_actor(self, name, actor):
         self.actor[name] = actor
 
-    def add_matrix(self, name, data_matrix, datatype=np.uint16, decimate=1):
-        matrix = self.matrix[name] = data_matrix
+    def add_matrix(self, name, data_matrix, datatype=np.uint8, decimate=1):
+        self.matrix[name] = data_matrix
+        self.add_matrix_cut_planes(name, data_matrix, datatype, decimate)
 
-        nx, ny, nz = matrix.shape
-        data_string = matrix.tostring('F')
+    def add_matrix_cut_planes(self, name, data_matrix, datatype=np.uint16, decimate=1):
+        self.reader[name] = reader = matrix_to_image_reader(name, data_matrix, datatype, decimate)
+        bwLut = define_LUT(data_matrix, "bw")
+        colorLut = define_LUT(data_matrix, "color")
+        for orientation in [1, 2, 3]:
+            actor, blend = blend_funct(data_matrix, reader, bwLut, reader, colorLut, orientation)
+            self.vtkdata['%s_blend_cut_plane_%d' % (name, orientation)] = blend
+            self.add_actor('%s_cut_plane_%d' % (name, orientation), actor)
 
-        reader = self.reader[name] = vtk.vtkImageImport()
-        reader.CopyImportVoidPointer(data_string, len(data_string))
-        reader.SetDataScalarTypeToUnsignedShort()
-        reader.SetNumberOfScalarComponents(1)
-        reader.SetDataExtent(0, nx - 1, 0, ny - 1, 0, nz - 1)
-        reader.SetWholeExtent(0, nx - 1, 0, ny - 1, 0, nz - 1)
+    def add_matrix_as_volume(self, name, data_matrix, datatype=np.uint16, decimate=1):
+        nx, ny, nz = data_matrix.shape
+        self.reader[name] = reader = matrix_to_image_reader(name, data_matrix, datatype, decimate)
 
         compositeFunction = vtk.vtkVolumeRayCastCompositeFunction()
         volumeMapper = vtk.vtkVolumeRayCastMapper()
