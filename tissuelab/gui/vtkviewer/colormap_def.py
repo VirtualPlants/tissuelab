a5d07f01e37aa000357eaba39953d09b12d7d7e4
Guillaume Baty <guillaume.baty@inria.fr>
2015-02-23 10:06:55 +0100

    Add colormap def

---
+++ tissuelab/gui/vtkviewer/colormap_def.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# -*- python -*-
+#
+#
+#       OpenAlea.OALab: Multi-Paradigm GUI
+#
+#       Copyright 2014 INRIA - CIRAD - INRA
+#
+#       File author(s): Guillaume Baty <guillaume.baty@inria.fr>
+#
+#       File contributor(s):
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+###############################################################################
+
+colormap_names = ["atmosphere",
+                  "blue",
+                  "cold",
+                  "color",
+                  "curvature",
+                  "density",
+                  "glasbey",
+                  "green",
+                  "grey",
+                  "hot",
+                  "invert_grey",
+                  "jet",
+                  "marocco",
+                  "ocean",
+                  "purple",
+                  "red",
+                  "sepia",
+                  "temperature",
+                  "terrain",
+                  "vegetation",
+                  ]
+colormap_names.sort()
