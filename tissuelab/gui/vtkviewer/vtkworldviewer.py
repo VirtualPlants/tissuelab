99f0ab6063ee3c2efafd19cd217bda2f15a89351
Nicolas Niclausse <Nicolas.Niclausse@inria.fr>
2017-01-27 11:24:58 +0100

    handle omero mimetype in drop event in vtkviewer

--- tissuelab/gui/vtkviewer/vtkworldviewer.py
+++ tissuelab/gui/vtkviewer/vtkworldviewer.py
@@ -22,6 +22,7 @@
 
 import vtk
 import numpy as np
+import re
 from scipy import ndimage as nd
 
 from openalea.core.observer import AbstractListener
@@ -656,7 +657,7 @@ class VtkWorldViewer(VtkViewer, AbstractListener):
             self.world.add(obj, **kwargs)
 
     # def dragEnterEvent(self, event):
-    #     for fmt in ['text/uri-list', 'openalealab/data']:
+    #     for fmt in ['text/uri-list', 'openalealab/data', 'openalealab/omero']:
     #         if event.mimeData().hasFormat(fmt):
     #             event.acceptProposedAction()
     #             return
@@ -664,7 +665,7 @@ class VtkWorldViewer(VtkViewer, AbstractListener):
     #     return QtGui.QWidget.dragEnterEvent(self, event)
 
     # def dragMoveEvent(self, event):
-    #     for fmt in ['text/uri-list', 'openalealab/data']:
+    #     for fmt in ['text/uri-list', 'openalealab/data', 'openalealab/omero']:
     #         if event.mimeData().hasFormat(fmt):
     #             event.acceptProposedAction()
     #             return
@@ -693,6 +694,16 @@ class VtkWorldViewer(VtkViewer, AbstractListener):
     #         from openalea.image.serial.basics import imread
     #         matrix = imread(data.path)
     #         self.world.add(matrix, name=data.path.namebase)
+    #         self.auto_focus()
+    #         event.acceptProposedAction()
+
+    #     elif source.hasFormat('openalealab/omero'):
+    #         from openalea.core.service.ipython import interpreter
+    #         db = interpreter().user_ns['omerodb']
+    #         uri = source.data('openalealab/omero')
+    #         uid = re.match(".*id=(\d+)", uri).groups()[0]
+    #         matrix = db.get_image(uid=uid)
+    #         self.world.add(matrix, name=source.text())
     #         self.auto_focus()
     #         event.acceptProposedAction()
 
